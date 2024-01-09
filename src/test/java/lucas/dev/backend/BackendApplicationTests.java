227f4d4db48fad2b141042b057e14714b29080ab
lkaminskii <lucaskaminskith@gmail.com>
2024-01-09 10:39:14 -0300

    Projeto gerado com Spring Initializr

---
+++ src/test/java/lucas/dev/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lucas.dev.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
